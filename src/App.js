1e5df2d59a2108a18bd50abac3290b2b6cae7826
Christian Sanches <christian@endless-point.org>
2019-10-03 20:05:23 -0300

    console.log hello world

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import api from './services/api'
 import './App.css';
 import logo from './assets/logo.svg'
 
 function App() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(' hello world')
+  }
+
+
   return (
     <div className="container">
       <img src={logo} alt="AirCNC"/>
@@ -11,7 +18,7 @@ function App() {
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
         </p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">Email *</label>
           <input type="email" name="email" id="email" placeholder="Seu melhor email!"/>
           <button className="btn" type="submit">Entrar</button>
