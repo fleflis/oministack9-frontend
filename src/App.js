392968002d554c61cbfc5fb90b1bac7f05a40eba
Christian Sanches <christian@endless-point.org>
2019-10-03 08:34:02 -0300

    finished adding content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
 
 function App() {
   return (
-    <div>
-      
+    <div className="container">
+      <img src="{logo}" alt="AirCNC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
+        </p>
+        <form>
+          <label htmlFor="email">Email *</label>
+          <input type="email" name="email" id="email" placeholder="Seu melhor email!"/>
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
