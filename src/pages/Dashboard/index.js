f2c21ecc415c988b136ae10a0cc852522284cb09
Christian Sanches <christian@endless-point.org>
2019-10-04 08:41:07 -0300

    dasbhoard and stuff for connecting to api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,22 @@
-import React from 'react' ;
-
+import React, { useEffect } from 'react' ;
+import api from '../../services/api';
 export default function Dashboard() {
-    return <h1>Dashboard</h1>
+    useEffect(() => {
+        
+        async function loadSpots() {
+            const _id = localStorage.getItem('user')
+            const response = await api.get('/dashboard',{
+                headers: {
+                    user_id : _id
+                }
+            })
+            console.log(response);
+        }
+
+        loadSpots();
+    }, [])
+
+    return (
+        <h1>Dashboard</h1>
+    )
 }
