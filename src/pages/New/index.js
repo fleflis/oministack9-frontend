b7e171cf90af2435faaa0a14e48d62f99c42d14d
Christian Sanches <christian@endless-point.org>
2019-10-05 17:30:14 -0300

    finished styling

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react' ;
+import React, { useState, useMemo } from 'react' ;
+import camera from '../../assets/camera.svg'
+import './styles.css'
 
 export default function New() {
     const [company, setCompany] = useState('')
     const [techs, setTechs] = useState('')  
     const [price, setPrice] = useState('')
+    const [thumbnail, setThumbnail] = useState(null)
+
+    const preview = useMemo( () => {
+        return thumbnail ? URL.createObjectURL(thumbnail) : null
+    }, [thumbnail] )
+
     function handleSubmit() {
 
     }
@@ -11,6 +19,12 @@ export default function New() {
 
     return (
         <form onSubmit={handleSubmit}>
+            <label id="thumbnail" style={{backgroundImage: `url(${preview})`}} className={thumbnail ? 'has-thumbnail' : ''}>
+                <input type="file" onChange={event => setThumbnail(event.target.files[0])} />
+                <img src={camera} alt="Select img"/>
+            </label>
+
+
             <label htmlFor="company">EMPRESA *</label>
             <input type="text" id="company" placeholder="Sua empresa incrível" value={company} onChange={event => setCompany(event.target.value)} />
             <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por vírgula)</span></label>
